9afef99ae7f4c22e427b553dbe228d3703029561
bevteo <beverleyteo1403@gmail.com>
2023-06-23 11:23:38 +0800

    Created handleEventLongPress hook

--- components/weekly page components/myEventComponent.js
+++ components/weekly page components/myEventComponent.js
@@ -4,8 +4,10 @@ import WeekView from 'react-native-week-view';
 
 const MyEventComponent = ({ event }) => (
   <>
-    <Text style={{ fontSize: 30, fontWeight: 'bold' }}>{event.title}</Text>
-    <Text style={{ fontSize: 16, }}>{event.description}</Text>
+    {/* <Text style={{ fontSize: 30, fontWeight: 'bold' }}>{event.title}</Text> */}
+    <Text style={{ fontSize: 15, fontFamily: 'spacemono-bold' }}>{event.description}</Text>
+    {/* <Text style={{ fontSize: 15, fontFamily: 'spacemono-bold' }} >{event.startDate}</Text>
+    <Text style={{ fontSize: 15, fontFamily: 'spacemono-bold' }}>{event.endDate}</Text> */}
   </>
 );
 
