120b53ba573abca33ba7a5d91fdf27ec36b86f5a
Lim Pei En <limpeien123@gmail.com>
2023-07-04 12:53:38 +0800

    implemented nusmods ics file parser and uploading of file info to database

--- screens/profile.js
+++ screens/profile.js
@@ -5,13 +5,16 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { getAuth, signOut } from "firebase/auth";
 
 import { db } from '../firebaseConfigDB';
-import { collection, query, where, getDocs, setDoc, doc } from "firebase/firestore";
+import { collection, query, where, getDocs, setDoc, doc, connectFirestoreEmulator } from "firebase/firestore";
 
 import * as DocumentPicker from 'expo-document-picker';
-// import ICAL from 'ical.js';
-// import { parseString } from 'cal-parser';
-import { parseString } from 'cal-parser'; 
-import { RRule } from 'rrule';
+import { RRule, RRuleSet, datetime, rrulestr } from 'rrule';
+import * as FileSystem from 'expo-file-system';
+import moment from 'moment-timezone';
+import { event } from 'react-native-reanimated';
+
+import { generateUUID } from '../hook/generateUUID';
+import { getMultiSectionDigitalClockUtilityClass } from '@mui/x-date-pickers';
 
 export default function ProfileScreen({navigation}) {
 
@@ -19,22 +22,23 @@ export default function ProfileScreen({navigation}) {
   const userEmail = auth.currentUser.email;
 
   const [userData, setUserData] = useState({});
-  const [timetable, setTimetable] = useState({}); 
-
-  const ical = require('cal-parser'); 
 
   // Function to retrieve user's name from firestore using their unique email address
   const getUserInfo = async () => {
-    const q = query(collection(db, "users"), where("email", "==", userEmail));
-    const querySnapShot = await getDocs(q);
-    querySnapShot.forEach((doc) => {
-      //console.log(doc.id, " => ", doc.data());
-      setUserData({
-        ...doc.data(),
-        id: doc.id,
-      })
-    });
-    //console.log(userData);
+    try {
+      const q = query(collection(db, "users"), where("email", "==", userEmail));
+      const querySnapShot = await getDocs(q);
+      querySnapShot.forEach((doc) => {
+        //console.log(doc.id, " => ", doc.data());
+        setUserData({
+          ...doc.data(),
+          id: doc.id,
+        })
+      });
+    } catch (error) {
+      console.log('Error retrieving user info: ', error);
+    }
+
   };
 
   // handles side effect from changing the state of object
@@ -53,15 +57,215 @@ export default function ProfileScreen({navigation}) {
     }
   };
 
-  const addTimetable = async () => {
-    try {
-      await setDoc(doc(db, "users", userEmail), { timetable: timetable }, { merge: true });
-      //   console.log("New Document for user created");
-    } catch(error) {
-        console.log(error);
+  function parseICS(icsData) {
+    const lines = icsData.split('\n');
+    const events = [];
+    let event = null;
+  
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].trim();
+  
+      if (line.startsWith('BEGIN:VEVENT')) {
+        event = {};
+      } else if (line.startsWith('END:VEVENT')) {
+        event.rrule = event.rrule || [];
+        event.exdates = event.exdates || [];
+        events.push(event);
+        event = null;
+      } else if (event) {
+        const [key, value] = line.split(':');
+  
+        switch (key) {
+          case 'SUMMARY':
+            event.summary = value;
+            break;
+          case 'LOCATION':
+            event.location = value;
+            break;
+          case 'DTSTART':
+            event.dtstart = value;
+            break;
+          case 'DTEND':
+            event.dtend = value;
+            break;
+          case 'RRULE':
+            event.rrule = value;
+            break;
+          case 'EXDATE':
+            event.exdates = event.exdates || [];
+            event.exdates.push(value);
+            break;
+          
+          default:
+            // Ignore unrecognized properties
+            break;
+        }
+      }
     }
+  
+    return events;
+  }
+  
+  // Helper function to convert UTC date to Singapore time zone
+  const convertToSingaporeTimeZone = (utcDate) => {
+    return moment.tz(utcDate, 'UTC').tz('Asia/Singapore');
+  };
+
+  // Helper function to get all recurring dates based on rrule and exdates
+  async function getAllRecurringDates(start, rrule, exdates) {
+    console.log('hi');
+
+    console.log('start: ', start);
+    console.log('rrule: ', rrule);
+    console.log('exdates: ', exdates);
+
+    let newRRule = rrule;
+    let freq = null;
+    let count = null; 
+    let byday = null;
+
+    // extracting freq, count and day of the week from rrule
+    if (newRRule.startsWith('FREQ=WEEKLY;')) {
+      newRRule = newRRule.slice(12);
+      freq = RRule.WEEKLY;
+    }
+
+    if (newRRule.startsWith('COUNT=')) {
+      newRRule = newRRule.slice(6);
+      count = newRRule.substring(0, newRRule.indexOf(';'));
+      console.log('COUNT: ', count);
+      console.log('newRRule: ', newRRule);
+      newRRule = newRRule.slice(0, newRRule.indexOf(';') + 1);
+      console.log('sliced: ', newRRule);
+    }
+
+    if (newRRule.startsWith('BYDAY=')) {
+      newRRule = newRRule.slice(6);
+      if (newRRule === 'MO') {
+        byday = 1;
+      } else if (newRRule === 'TU') {
+        byday = 2;
+      } else if (newRRule === 'WE') {
+        byday = 3;
+      } else if (newRRule === 'TH') {
+        byday = 4;
+      } else if (newRRule === 'FR') {
+        byday = 5;
+      } else if (newRRule === 'SA') {
+        byday = 6;
+      } else if (newRRule === 'SU') {
+        byday = 7;
+      }
+      console.log('byday: ', byday);
+    }
+
+    // creation of RRuleSet for this particular module's class
+    const rs = new RRuleSet();
+
+    // convert starting date to sgt, string representation. 
+    console.log(start);
+    const dtstart = convertToSingaporeTimeZone(start).toString();
+    console.log('dtstart: ', dtstart);
+    // const bool = typeof dtstart === 'string';
+    // console.log(bool);
+
+    rs.rrule(
+      new RRule({ 
+        dtstart: new Date(dtstart),
+        freq: freq, 
+        count: count,
+        byweekday: byday,
+      })
+    );
+
+    // convert exdates to sgt and suitable format and add to the RRuleSet rs 
+    const convertedExdates = exdates.map((date) => new Date(convertToSingaporeTimeZone(date)));
+    for (let i = 0; i < convertedExdates.length; i++) {
+      rs.exdate(convertedExdates[i]);
+    }
+    
+    // generate all dates and return 
+    return rs.all();
+  };
+
+  // helper function to process and upload classes/exams from nusmods tt to our database
+  async function handleClassEventCreation(name, dtstart, dtend, rrule, exdates) {
+
+    // class type events: lectures, tutorials, recitations etc with recurring dates 
+    if (exdates.length > 0 && rrule.length > 0) {
+
+      console.log('this is a class: ', name);
+      const category = 'class';
+
+      const startTime = dtstart.slice(9, 15);
+      const endTime = dtend.slice(9, 15);
+      const duration = endTime - startTime;
+      const durationInHours = duration / 10000;
+
+      const dates = await getAllRecurringDates(dtstart, rrule, exdates);
+      console.log(dates);
+
+      for (let i = 0; i < dates.length; i++) {
+        const startDateTime = dates[i];
+        console.log('SDT: ', startDateTime);
+
+        let edt = startDateTime;
+        const temp = moment(edt).add(durationInHours, 'hours').toDate();
+        console.log('temp: ', temp);
+        const endDateTimeStr = temp.toString();
+      
+        // upload class event to database
+        try {
+          const uniqueID = generateUUID(15);
+          const docRef = doc(db, 'users', userEmail, "events", uniqueID);
+          await setDoc(docRef, {
+            id: uniqueID,
+            description: name,
+            startDate: startDateTime,
+            endDate: new Date(endDateTimeStr),
+            category: category,
+            colour: '#9AC791', 
+            completed: false, 
+          });
+
+        } catch(error) {
+          console.log(error);
+        }
+      }
+
+    // one time events: final exam
+    } else {
+      console.log('this is an exam: ', name);
+      const category = 'event';
+
+      const startdt = convertToSingaporeTimeZone(dtstart);
+      const startDateTime = new Date(startdt);
+
+      const enddt = convertToSingaporeTimeZone(dtend);
+      const endDateTime = new Date(enddt);
+
+      // upload exam event to database
+      try {
+        const uniqueID = generateUUID(15);
+        const docRef = doc(db, 'users', userEmail, "events", uniqueID);
+        await setDoc(docRef, {
+          id: uniqueID,
+          description: name,
+          startDate: startDateTime,
+          endDate: endDateTime,
+          category: category,
+          colour: '#E5E5E5', 
+          completed: false, 
+        });
+        
+      } catch(error) {
+        console.log(error);
+      }
+    }
+
   }
 
+  // handle selection of ics file that has been downloaded from nusmods by user
   const pickDoc = async () => {
     try {
       const file = await DocumentPicker.getDocumentAsync({
@@ -72,35 +276,32 @@ export default function ProfileScreen({navigation}) {
         throw new Error('wrong file type, please select an ics file');
       } 
 
-      const fileContents = await fetch(file.uri).then((response) => response.text());
-      // console.log(fileContents);
+      console.log('new');
 
-      const parsedData = parseString(fileContents);
-      console.log(parsedData.events);
+      const fileContents = await FileSystem.readAsStringAsync(file.uri);
+      console.log(fileContents);
 
-      parsedData.events.map(event => {
-        const desc = event.description;
-        const startdt = event.dtstart; 
-        const recurrenceRule = event.recurrenceRule;
-        //const freq = recurrenceRule.options.freq;
-        //const interval = recurrenceRule.origOptions.interval;
-        //const byweekday = recurrenceRule.origOptions.byweekday;
+      try {
+        const ev = parseICS(fileContents);
+        // console.log('ev: ', ev);
 
-        //console.log(recurrenceRule); 
+        for (const mod of ev) {
+          const name = mod.summary;
+          const location = mod.location; 
+          const dtstart = mod.dtstart;
+          const dtend = mod.dtend;
+          const rrule = mod.rrule;
+          const exdates = mod.exdates;
 
-        //console.log(desc);
-        //console.log(startdt);
-        //console.log(freq);
-        //console.log(interval);
-        //console.log(byweekday);
-        //console.log(freq);
-        //console.log(interval);
-      });
+          await handleClassEventCreation(name, dtstart, dtend, rrule, exdates);
+          console.log('next');
+        };
 
-  
-      // setTimetable(parsedData.events);
-      // await addTimetable(); 
-      // console.log(parsedData.events);
+        return;
+
+      } catch (error) {
+        console.log('Error parsing ICS file: ', error);
+      }
      
     } catch (error) {
       Alert.alert('Something went wrong: ', error); 
