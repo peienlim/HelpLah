5fc585244517ab4fa8219becd61c5733d89d60bb
Lim Pei En <limpeien123@gmail.com>
2023-07-15 22:57:53 +0800

    statistics page rough layout

---
+++ components/stats page components/componentA.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function DailyComponent() {
+
+    return (
+        <View>
+            <Text>Component A</Text>
+        </View>
+    )
+} 
+
